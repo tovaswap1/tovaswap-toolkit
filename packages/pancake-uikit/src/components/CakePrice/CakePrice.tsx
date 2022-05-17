f721dca33c311e5af1e60dfa015083148b21b809
christian <101934549+tovaswap1@users.noreply.github.com>
2022-05-16 21:24:51 -0500

    Cambios

---
+++ packages/pancake-uikit/src/components/CakePrice/CakePrice.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import LogoRound from "../Svg/Icons/LogoRound";
+import Text from "../Text/Text";
+import Skeleton from "../Skeleton/Skeleton";
+import { Colors } from "../../theme";
+
+export interface Props {
+  color?: keyof Colors;
+  cakePriceUsd?: number;
+}
+
+const PriceLink = styled.a`
+  display: flex;
+  align-items: center;
+  svg {
+    transition: transform 0.3s;
+  }
+  :hover {
+    svg {
+      transform: scale(1.2);
+    }
+  }
+`;
+
+const CakePrice: React.FC<Props> = ({ cakePriceUsd, color = "textSubtle" }) => {
+  return cakePriceUsd ? (
+    <PriceLink
+      href="https://www.tovaswap.com/swap?outputCurrency=0x6f1b5EA075e19c705ef9c1400f4dF9000a713930"
+      target="_blank"
+    >
+      <LogoRound width="24px" mr="8px" />
+      <Text color={color} bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
+    </PriceLink>
+  ) : (
+    <Skeleton width={80} height={24} />
+  );
+};
+
+export default React.memo(CakePrice);
